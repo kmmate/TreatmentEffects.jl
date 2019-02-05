82238d338e51ee0d9586699a916cfceb4ed2a0a9
kmmate <mate.kormos33@gmail.com>
2019-02-05 13:21:40 +0100

    fix to drop_missing

--- src/data_cleaning.jl
+++ src/data_cleaning.jl
@@ -120,10 +120,10 @@ function drop_missing(y::Array{<:Union{Missing, Any}, 1},
 	x::Array{<:Union{Missing, Any}, 2})
 	# indices with not missing entries
 	# check y and d
-	not_missing_idx = (isequal.(y, missing) .== false) .* (isequal.(d, missing) .== false)
+	notmissing_idx = (isequal.(y, missing) .== false) .* (isequal.(d, missing) .== false)
 	# check x's
 	for col in 1:size(x)[2]
-		not_missing_idx = not_missing_idx .* (isequal.(x[:, col], missing) .==false)
+		notmissing_idx = not_missing_idx .* (isequal.(x[:, col], missing) .==false)
 	end
 	y_dropped_uniontype = y[notmissing_idx]
 	d_dropped_uniontype = d[notmissing_idx]
