ff434dfd488988d3c3713389a1aa2cfd312cc98d
kmmate <mate.kormos33@gmail.com>
2019-02-15 23:56:17 +0100

    exported nonparametrics

--- src/TreatmentEffects.jl
+++ src/TreatmentEffects.jl
@@ -1,19 +1,32 @@
 module TreatmentEffects
 
 export
+	# data cleaning
 	drop_missing,
+	# Exogenous Participation
 	ExogenousParticipationModel,
 	ate_estimator,
 	bootstrap_distribution,
 	bootstrap_htest,
+	# Matching
 	MatchingModel,
 	ate_matchingestimator,
 	att_matchingestimator,
-	ate_blockingestimator
+	ate_blockingestimator,
+	# nonparametric
+	uniform_kernel,
+	triangular_kernel,
+	gaussian_kernel,
+	epanechnikov_kernel,
+	localpoly_regression,
+	polynomial_features
+
 
 include("data_cleaning.jl")
 include("modeltypes.jl")
 include("estimators.jl")
+include("nonparametrics\\kernels.jl")
+include("nonparametrics\\np_regression.jl")
 include("exogenous_participation.jl")
 include("matching.jl")
 end
